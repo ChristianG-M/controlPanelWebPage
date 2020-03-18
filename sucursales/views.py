a9f4e5e240b31755610eda4f46b22256af2ffb1c
Christian <christian.gm1719@gmail.com>
2020-03-18 13:19:51 -0600

    CRUD funcional del modulo Sucursal

--- sucursales/views.py
+++ sucursales/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from .models import Sucursal
 
@@ -32,4 +32,34 @@ class SucursalCreate(CreateView):
         'image',
         'supervisor'        
     ]
+    success_url = reverse_lazy('Sucursal:Sucursales')
+
+class SucursalUpdate(UpdateView):
+    model = Sucursal
+    fields = [
+        'nombre',
+        'empresa',
+        'latitud',
+        'longitud',
+        'pais',
+        'estado',
+        'alcaldia',
+        'codigoPostal',
+        'colonia',
+        'calle',
+        'numero',
+        'telefono',
+        'correo',
+        'horaApertura',
+        'horaCierre',
+        'image',
+        'supervisor'        
+    ]
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self):
+        return reverse_lazy('Sucursal:Update', args=[self.object.id]) + '?ok'
+
+class SucursalDelete(DeleteView):
+    model = Sucursal
     success_url = reverse_lazy('Sucursal:Sucursales')
