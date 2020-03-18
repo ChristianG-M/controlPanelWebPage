c33aa18dd1291bda1a1b8a9367b3003760a6b3e2
Christian <christian.gm1719@gmail.com>
2020-03-18 17:42:17 -0600

    Botones de inicio y registro redireccionando a login

--- core/views.py
+++ core/views.py
@@ -5,7 +5,4 @@ from django.views.generic.base import TemplateView
 class HomePageView(TemplateView):
     template_name = "core/home.html"
 
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'title':'Mi Super Web Get Method'})
-
 # Create your views here.
